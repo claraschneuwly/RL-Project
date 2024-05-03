4682714152405193d7e554586c648b9d4d9906d5
Clara Schneuwly <clarasch@mit.edu>
2024-05-03 09:44:21 -0400

    Cleaned code

--- TD3_main.py
+++ TD3_main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
-import utils_TD3
+import ReplayBuffer
 import TD3
 from FinalEnv import *
 
+# Parameters for the environment
 env_param = dict(a=0, 
 				  T=1, 
 				  k=0.1, 
@@ -16,55 +18,32 @@ env_param = dict(a=0,
 				  y_goal=2, 
 				  pos0=np.array([0, 0, 0]), 
 				  theta0=0, 
-				  dist_threshold=0.3, 
-				  max_steps=200)
+				  dist_threshold=0.2, 
+				  max_steps=150)
 
-# Runs policy for X episodes and returns average reward
-# A fixed seed is used for the eval environment
-# def eval_policy(policy, env_name, seed, eval_episodes=10):
-def eval_policy(policy, env_param, eval_episodes=10):
-	eval_env = FluidMechanicsEnv(**env_param)
-	# eval_env.seed(seed + 100)
-
-	avg_reward = 0.
-	for _ in range(eval_episodes):
-		state, done = eval_env.reset(), False
-		while not done:
-			action = policy.select_action(np.array(state))
-			state, reward, _, done, _, _= eval_env.step(action)
-			avg_reward += reward
-
-	avg_reward /= eval_episodes
-
-	print("---------------------------------------")
-	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
-	print("---------------------------------------")
-	return avg_reward
-
+# Define parameters for Twin Delayed Deep Deterministic
 class Config:
     def __init__(self,
                 discount=0.995,
-                tau=0.005,
+                tau=0.005, 
                 policy_noise=0.2,
                 expl_noise=0.1,
                 noise_clip=0.5,
-                eval_freq=2,
                 policy_freq=2,
-                max_timesteps=10000,
-                start_timesteps= 1000,
-                batch_size=50,
+                max_timesteps=1e6,
+                start_timesteps=25e3,
+                batch_size=256,
                 seed=0):
 		
         self.discount = discount # discount factor
-        self.tau = tau
-        self.policy_noise = policy_noise
-        self.eval_freq = eval_freq
-        self.policy_freq = policy_freq
-        self.expl_noise = expl_noise
-        self.noise_clip = noise_clip
-        self.max_timesteps = max_timesteps
-        self.start_timesteps = start_timesteps
-        self.batch_size = batch_size
+        self.tau = tau # Target network update rate
+        self.policy_noise = policy_noise # Noise added to target policy during critic update
+        self.policy_freq = policy_freq  # Frequency of delayed policy updates
+        self.expl_noise = expl_noise # Std of Gaussian exploration noise
+        self.noise_clip = noise_clip # Range to clip target policy noise
+        self.max_timesteps = max_timesteps # Max time steps to run environment
+        self.start_timesteps = start_timesteps # Time steps during which initial random policy is used, after that we have collected sufficient data to train the agent 
+        self.batch_size = batch_size # Batch size for both actor and critic
         self.seed = seed
 
 if __name__ == "__main__":
@@ -74,8 +53,6 @@ if __name__ == "__main__":
 	env = FluidMechanicsEnv(**env_param)
 
 	# Set seeds
-	# env.seed(args.seed)
-	# env.action_space.seed(args.seed)
 	torch.manual_seed(args.seed)
 	np.random.seed(args.seed)
 	random.seed(args.seed)
@@ -98,15 +75,13 @@ if __name__ == "__main__":
 	kwargs["policy_freq"] = args.policy_freq
 	policy = TD3.TD3(**kwargs)
 
-	replay_buffer = utils_TD3.ReplayBuffer(state_dim, action_dim)
-	
-	# Evaluate untrained policy
-	# evaluations = [eval_policy(policy, env_param)] #, args.seed
+	replay_buffer = ReplayBuffer.ReplayBuffer(state_dim, action_dim)
 
 	state, done = env.reset(), False
 	episode_reward = 0
 	episode_timesteps = 0
 	episode_num = 0
+	cumulative_episode_timesteps = 0 # track the time steps for plot
 
 	for t in range(int(args.max_timesteps)):
 		
@@ -115,37 +90,49 @@ if __name__ == "__main__":
 		# Select action randomly or according to policy
 		if t < args.start_timesteps:
 			action = env.action_space_sample()
+			print("action random ",action)
+
 		else:
 			action = (
 				policy.select_action(np.array(state))
 				+ np.random.normal(0, max_action * args.expl_noise, size=action_dim)
 			).clip(-max_action, max_action)
+			print("action ",action)
 
 		# Perform action
-		next_state, reward, _, done, _, _ = env.step(action) 
+		next_state, reward, _, done, _, _ = env.step(action)
 		done_bool = float(done) if episode_timesteps < env.max_steps else 0
-
 		# Store data in replay buffer
 		replay_buffer.add(state, action, next_state, reward, done_bool)
-
 		state = next_state
 		episode_reward += reward
 
 		# Train agent after collecting sufficient data
 		if t >= args.start_timesteps:
 			policy.train(replay_buffer, args.batch_size)
-
-		if done: 
+		if done:
 			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
 			print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
-			# Reset environment
+			if episode_num >= 250: # Plot once the agent has learned enough
+				x = replay_buffer.next_state[cumulative_episode_timesteps:cumulative_episode_timesteps+episode_timesteps, 0]
+				y = replay_buffer.next_state[cumulative_episode_timesteps:cumulative_episode_timesteps+episode_timesteps, 1]
+				theta = replay_buffer.next_state[cumulative_episode_timesteps:cumulative_episode_timesteps+episode_timesteps, 3]
+				x = np.insert(x, 0, 0) # Add the first position (0,0,0) for the plot
+				y = np.insert(y, 0, 0)
+				theta = np.insert(theta, 0, 0)
+				fig = plt.figure(figsize = (14, 12))
+				plt.grid(True)
+				plt.scatter([env.x_goal], [env.y_goal], marker = "o", color = "r")
+				plt.plot(x, y, 'k-o')
+				plt.show()
+
+            # Reset environment
 			state, done = env.reset(), False
 			episode_reward = 0
+			cumulative_episode_timesteps += episode_timesteps
 			episode_timesteps = 0
-			episode_num += 1 
+			episode_num += 1		
 
-		# Evaluate episode
-		# if (t + 1) % args.eval_freq == 0:
-		# 	evaluations.append(eval_policy(policy, env_param))
-		# 	# np.save(f"./results/{file_name}", evaluations)
-		# 	# if args.save_model: policy.save(f"./models/{file_name}")
+
+
+#Source: https://github.com/sfujim/TD3/blob/master/TD3.py
