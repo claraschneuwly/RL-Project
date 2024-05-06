1a915b8fc68c13e721c295277bfaf8092c0b6b77
valentin-brekke <valentin.brekke@student-cs.fr>
2024-05-05 20:27:24 -0400

    SAC py framework

---
+++ SAC.py
@@ -0,0 +1,236 @@
+import os
+import random
+import time
+from dataclasses import dataclass
+
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+
+
+class SoftQNetwork(nn.Module):
+    def __init__(self, env):
+        super().__init__()
+        self.fc1 = nn.Linear(np.array(env.observation_space.shape).prod() + np.prod(env.action_space.shape), 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.fc2 = nn.Linear(256, 256)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.fc3 = nn.Linear(256, 1)
+
+    def forward(self, x, a):
+        x = torch.cat([x, a], 1)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+
+
+LOG_STD_MAX = 2
+LOG_STD_MIN = -5
+
+class Actor(nn.Module):
+    def __init__(self, env):
+        super().__init__()
+        self.fc1 = nn.Linear(np.prod(env.observation_space.shape), 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc_mean = nn.Linear(256, np.prod(env.action_space.shape))
+        self.fc_logstd = nn.Linear(256, np.prod(env.action_space.shape))
+        # action rescaling
+        self.register_buffer(
+            "action_scale", torch.tensor((env.action_space.high - env.action_space.low) / 2.0, dtype=torch.float32)
+        )
+        self.register_buffer(
+            "action_bias", torch.tensor((env.action_space.high + env.action_space.low) / 2.0, dtype=torch.float32)
+        )
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mean = self.fc_mean(x)
+        log_std = self.fc_logstd(x)
+        log_std = torch.tanh(log_std)
+        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)  # From SpinUp / Denis Yarats
+
+        return mean, log_std
+
+    def get_action(self, x):
+        mean, log_std = self(x)
+        std = log_std.exp()
+        normal = torch.distributions.Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        y_t = torch.tanh(x_t)
+        action = y_t * self.action_scale + self.action_bias
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + 1e-6)
+        log_prob = log_prob.sum(-1, keepdim=True)
+        mean = torch.tanh(mean) * self.action_scale + self.action_bias
+        return action, log_prob, mean
+
+
+class ReplayBuffer(object):
+	def __init__(self, state_dim, action_dim, max_size=int(1e9)):
+		self.max_size = max_size
+		self.ptr = 0
+		self.size = 0
+
+		self.state = np.zeros((max_size, state_dim))
+		self.action = np.zeros((max_size, action_dim))
+		self.next_state = np.zeros((max_size, state_dim))
+		self.reward = np.zeros((max_size, 1))
+		self.not_done = np.zeros((max_size, 1))
+
+		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+	def add(self, state, action, next_state, reward, done):
+		self.state[self.ptr] = state
+		self.action[self.ptr] = action
+		self.next_state[self.ptr] = next_state
+		self.reward[self.ptr] = reward
+		self.not_done[self.ptr] = 1. - done
+
+		self.ptr = (self.ptr + 1) % self.max_size
+		self.size = min(self.size + 1, self.max_size)
+
+
+	def sample(self, batch_size):
+		ind = np.random.randint(0, self.size, size=batch_size)
+
+		return (
+			torch.FloatTensor(self.state[ind]).to(self.device),
+			torch.FloatTensor(self.action[ind]).to(self.device),
+			torch.FloatTensor(self.next_state[ind]).to(self.device),
+			torch.FloatTensor(self.reward[ind]).to(self.device),
+			torch.FloatTensor(self.not_done[ind]).to(self.device)
+		)
+
+
+class SAC:
+    def __init__(self, env, args):
+        self.env = env
+        self.args = args
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+        self.actor = Actor(env).to(self.device)
+        self.qf1 = SoftQNetwork(env).to(self.device)
+        self.qf2 = SoftQNetwork(env).to(self.device)
+        self.qf1_target = SoftQNetwork(env).to(self.device)
+        self.qf2_target = SoftQNetwork(env).to(self.device)
+        self.qf1_target.load_state_dict(self.qf1.state_dict())
+        self.qf2_target.load_state_dict(self.qf2.state_dict())
+
+        self.q_optimizer = optim.Adam(list(self.qf1.parameters()) + list(self.qf2.parameters()), lr=args.q_lr)
+        self.actor_optimizer = optim.Adam(list(self.actor.parameters()), lr=args.policy_lr)
+
+        if args.autotune:
+            self.target_entropy = -torch.prod(torch.Tensor(env.action_space.shape).to(self.device)).item()
+            self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+            self.alpha = self.log_alpha.exp().item()
+            self.a_optimizer = optim.Adam([self.log_alpha], lr=args.q_lr)
+        else:
+            self.alpha = args.alpha
+
+        self.replay_buffer = ReplayBuffer(env.observation_space.shape[0], env.action_space.shape[0])
+
+    def train(self):
+        obs = self.env.reset()
+        start_time = time.time()
+        episode_num = 0
+        episode_reward = 0
+        history_reward = []
+        smooth_reward = []
+        SMOOTH_REWARD_WINDOW = 50
+
+        for global_step in range(self.args.total_timesteps):
+
+            if global_step < self.args.learning_starts:
+                actions = np.array(self.env.action_space.sample())
+            else:
+                actions, _, _ = self.actor.get_action(torch.Tensor(obs).to(self.device))
+                actions = actions.detach().cpu().numpy()
+
+            next_obs, reward, sum_reward, done, steps_count, all_actions = self.env.step(actions)
+
+            self.replay_buffer.add(obs, actions, next_obs, reward, done)
+            
+            obs = next_obs
+            episode_reward += reward
+            if done:
+                obs = self.env.reset()
+                episode_num += 1
+                history_reward.append(episode_reward)
+                smooth_reward.append(np.mean(history_reward[-SMOOTH_REWARD_WINDOW:]))
+
+            if global_step > self.args.learning_starts:
+                data = self.replay_buffer.sample(self.args.batch_size)
+                self.update_parameters(data, global_step, start_time)
+
+        execution_time = time.time() - start_time
+
+        return self.actor, self.args.total_timesteps, episode_num, smooth_reward, execution_time
+
+
+
+    def update_parameters(self, data, global_step, start_time):
+        # Update Q functions
+        with torch.no_grad():
+            next_state_actions, next_state_log_pi, _ = self.actor.get_action(data[2])
+            qf1_next_target = self.qf1_target(data[2], next_state_actions)
+            qf2_next_target = self.qf2_target(data[2], next_state_actions)
+            min_qf_next_target = torch.min(qf1_next_target, qf2_next_target) - self.alpha * next_state_log_pi
+            next_q_value = data[3].flatten() + (1 - data[4].flatten()) * self.args.gamma * min_qf_next_target.view(-1)
+
+        qf1_a_values = self.qf1(data[0], data[1]).view(-1)
+        qf2_a_values = self.qf2(data[0], data[1]).view(-1)
+        qf_loss = F.mse_loss(qf1_a_values, next_q_value) + F.mse_loss(qf2_a_values, next_q_value)
+
+        self.q_optimizer.zero_grad()
+        qf_loss.backward()
+        self.q_optimizer.step()
+
+        # Policy update and target network update
+        if global_step % self.args.policy_frequency == 0:
+            for _ in range(self.args.policy_frequency):  # TD 3 style delayed update
+                pi, log_pi, _ = self.actor.get_action(data[0])
+                qf1_pi = self.qf1(data[0], pi)
+                qf2_pi = self.qf2(data[0], pi)
+                min_qf_pi = torch.min(qf1_pi, qf2_pi)
+                actor_loss = ((self.alpha * log_pi) - min_qf_pi).mean()
+
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+
+                if self.args.autotune:
+                    with torch.no_grad():
+                        _, log_pi, _ = self.actor.get_action(data[0])
+                    alpha_loss = -(self.log_alpha.exp() * (log_pi + self.target_entropy)).mean()
+
+                    self.a_optimizer.zero_grad()
+                    alpha_loss.backward()
+                    self.a_optimizer.step()
+                    self.alpha = self.log_alpha.exp().item()
+
+        # Target networks soft update
+        if global_step % self.args.target_network_frequency == 0:
+            self.update_targets()
+
+        # Optional: Logging and print statements for monitoring
+        if global_step % 1000 == 0:
+            print(f"Step: {global_step}, QF Loss: {qf_loss.item() / 2.0}")
+            print(f"Step: {global_step}, Actor Loss: {actor_loss.item()}")
+
+    def update_targets(self):
+        for param, target_param in zip(self.qf1.parameters(), self.qf1_target.parameters()):
+            target_param.data.copy_(self.args.tau * param.data + (1 - self.args.tau) * target_param.data)
+        for param, target_param in zip(self.qf2.parameters(), self.qf2_target.parameters()):
+            target_param.data.copy_(self.args.tau * param.data + (1 - self.args.tau) * target_param.data)
+
+
+
---
+++ env.py
@@ -0,0 +1,270 @@
+## Env augmented to fit the SAC req
+# Added some action and obs boxes
+
+
+import numpy as np 
+import math
+from gym import spaces
+
+def angle_between_vectors(v1, v2):
+    dot_product = v1[0] * v2[0] + v1[1] * v2[1]
+    magnitude_v1 = math.sqrt(v1[0]**2 + v1[1]**2)
+    magnitude_v2 = math.sqrt(v2[0]**2 + v2[1]**2)
+    
+    if magnitude_v1 != 0 and magnitude_v2 != 0:
+        cos_theta = dot_product / (magnitude_v1 * magnitude_v2)
+        if cos_theta > 1:
+                cos_theta = 1
+        elif cos_theta < -1:
+            cos_theta = -1
+        angle_radians = math.acos(cos_theta)
+        return angle_radians
+    else:
+        return 0
+
+def normalize_vector_from_point(x, y, x0, y0):
+    # Calcul des composantes du vecteur
+    vector_x = x - x0
+    vector_y = y - y0
+    
+    # Calcul de la magnitude
+    magnitude = math.sqrt(vector_x**2 + vector_y**2)
+    
+    # Normalisation
+    if magnitude != 0:
+        normalized_x = vector_x / magnitude
+        normalized_y = vector_y / magnitude
+        return (normalized_x, normalized_y)
+    else:
+        return (0, 0)
+
+
+class FluidMechanicsEnv:
+    
+    class Wave:
+        def __init__(self, a, T, k) :
+            self.a = a                  # Wave amplitude
+            self.T = T                  # Wave period
+            self.omega = 2 * np.pi / T  # Wave frequency
+            self.k = .1   
+
+    class Wind:
+        def __init__(self, Ux, Uy, alpha, sigma) :
+            self.Ux = Ux                  # Wave amplitude
+            self.Uy = Uy                  # Wave period
+            self.alpha = alpha            # Wave frequency
+            self.sigma = sigma
+
+    def __init__(self, a, T, k,  Ux, Uy, alpha, sigma, x_goal, y_goal, pos0, theta0, dist_threshold=0.1, max_steps=1000, ocean=False, dt=1, max_thrust_speed=1):
+        self.t = 0
+        self.wave = self.Wave(a, T, k)
+        self.wind = self.Wind(Ux, Uy, alpha, sigma)
+        self.max_steps = max_steps
+        self.dist_threshold = dist_threshold
+        self.max_x, self.min_x = 100 , -100  # agent has drifted too far, admit defeat
+        self.max_y, self.min_y = 100 , -100 # agent has drifted too far, admit defeat
+        self.x_goal, self.y_goal, self.z_goal = x_goal, y_goal, 0 # coordinates of goal
+        self.dir_goal = normalize_vector_from_point(self.x_goal, self.y_goal, 0, 0)
+        self.done = False
+        self.goal_reached = False
+        self.steps_count = 0
+        self.sum_reward = 0
+        self.all_actions = []
+        self.pos = pos0
+        self.theta = theta0
+        self.vel = np.array([0, 0, 0]).astype(np.float32)
+        self.thrust = 0 # [0; 1]
+        self.rudder = 0.0 # [-pi/4; pi/4]
+        self.action = np.array([0, 0])
+        self.u_history = []
+        self.v_history = []
+        self.straight = False
+        self.alpha = 0.1
+        self.ocean = ocean
+        if self.ocean:
+            self.state_dim = 9 # u_water, v_water
+        else:
+            self.state_dim = 7  # x, y, z. 
+        self.action_dim = 2  # thrust, rudder angle
+        self.dt = dt
+        self.max_thrust_speed = max_thrust_speed
+
+        if self.ocean:
+            self.observation_space = spaces.Box(low=-100, high=100, shape=(9,), dtype=np.float32)
+        else:
+            self.observation_space = spaces.Box(low=-100, high=100, shape=(7,), dtype=np.float32)
+        
+        self.action_space = spaces.Box(low=np.array([0, -np.pi/4]), high=np.array([max_thrust_speed, np.pi/4]), dtype=np.float32)
+
+    def water_surface_level(self, pos) :
+        x, _, _ = pos
+        eta = self.wave.a * np.sin(self.wave.omega * self.t - self.wave.k * x)
+        return eta
+
+    def water_speed(self, pos) :
+        x, y, z = pos
+        eta = self.water_surface_level(pos)
+
+        u_swell = self.wave.a * self.wave.omega * np.exp(self.wave.k * z) * np.sin(self.wave.omega * self.t - self.wave.k * x)
+        w_swell = self.wave.a * self.wave.omega * np.exp(self.wave.k * z) * np.cos(self.wave.omega * self.t - self.wave.k * x)
+        
+        u_wind = np.random.normal(self.wind.Ux, self.wind.sigma) * np.exp(self.wind.alpha * (z-self.wave.a))
+        v_wind = np.random.normal(self.wind.Uy, self.wind.sigma) * np.exp(self.wind.alpha * (z-self.wave.a))
+
+        # u = u + np.random.normal(0, noise, u.shape)
+        # v = v + np.random.normal(0, noise, v.shape)
+        # w = w + np.random.normal(0, noise, w.shape)
+
+        return u_swell + u_wind, v_wind, w_swell
+
+    def inertia(self, lag = 3):
+
+        if len(self.u_history) > 0 :
+            k = np.minimum(lag, len(self.u_history))
+            coefs = np.array([1 / (4 ** (i + 1)) for i in reversed(range(k))])
+            u = (self.u_history[-k:] * coefs).sum()
+            v = (self.v_history[-k:] * coefs).sum()
+
+        else :
+            u, v = 0, 0
+
+        return np.array([u, v, 0])
+    
+    def update_pos(self, action):
+        # Sets agent action
+        self.thrust = action[0]*self.max_thrust_speed
+        self.rudder = action[1]
+    
+        # Find the water velocity at agent position
+        x, y, z = self.pos
+        u, v, w = self.water_speed(self.pos)
+        self.vel = np.array([u, v, w])
+
+        # Add inertia to the agent's velocity
+        self.vel += self.inertia()
+
+        # Perform agent action
+        self.theta += self.rudder 
+        self.theta %= (2*np.pi) # Update agent's orientation from rudder angle
+        u_action = self.thrust * np.cos(self.theta)
+        v_action = self.thrust * np.sin(self.theta)
+        self.vel += np.array([u_action, v_action, 0])
+
+        # Update velocity history
+        self.u_history.append(u)
+        self.v_history.append(v)
+
+        # Update agent position
+        x += self.vel[0]*self.dt
+        y += self.vel[1]*self.dt
+        z = self.water_surface_level((x, y, z))
+
+        # Lucas' alignement checks (Lucas double check stp)
+        self.dir_goal = normalize_vector_from_point(self.x_goal, self.y_goal, x, y)
+        if not self.straight and angle_between_vectors(self.dir_goal, (np.sin(self.theta), np.cos(self.theta))) < 2*self.alpha:
+            self.straight = True
+
+        return np.array([x, y, z])
+    
+    def get_reward(self):
+        
+        # Calculate euclidian dist to goal Without z coord
+        goal_pos = np.array([self.x_goal, self.y_goal])
+        dist_to_goal = np.linalg.norm(np.array(self.pos[:2]) - goal_pos)
+        dist_to_dir = angle_between_vectors(self.dir_goal, (np.sin(self.theta), np.cos(self.theta)))/np.pi
+        ##reward = - (dist_to_goal/100 + np.float64(dist_to_dir))/50
+
+        #TODO: write parametrize reward function based on reward position
+
+        reward = - (dist_to_goal/5000 + (np.exp((1 + np.float64(dist_to_dir))) - 1)/200)
+        if dist_to_goal <= self.dist_threshold:
+            reward += 10
+        if self.straight and angle_between_vectors(self.dir_goal, (np.sin(self.theta), np.cos(self.theta))) >= self.alpha:
+            reward -= 0
+        return reward
+    
+    def success(self):
+        """Returns True if x,y is near enough goal"""
+        goal_pos = np.array([self.x_goal, self.y_goal])
+        dist_to_goal = np.linalg.norm(np.array(self.pos[:2]) - goal_pos)
+        if  dist_to_goal <= self.dist_threshold:
+            return True
+        else:
+            return False
+        
+    def admit_defeat(self):
+        """Returns True if the agent has drifted too far away from goal"""
+        if self.pos[0] > self.max_x or self.pos[0] < self.min_x or self.pos[1] > self.max_y or self.pos[1] < self.min_y:
+            return True
+        else:
+            return False
+        
+    def step(self, action) :
+        
+        self.pos = self.update_pos(action)
+        self.reward = self.get_reward()
+        self.sum_reward += self.reward
+        self.steps_count += 1
+        self.all_actions += [action]
+        self.t += self.dt
+        if self.success():
+            self.done = True
+            self.goal_reached = True
+
+        elif self.admit_defeat() or self.steps_count > self.max_steps:
+            self.done = True
+        
+        if self.ocean: 
+            # TODO: Add noise to observation if algo works fine with perfect estimate of wave and winds speed
+
+            # TODO: separate wind and water component
+
+            u_water, v_water, _ = self.water_speed(self.pos)
+            return np.concatenate((self.pos, np.array([np.cos(self.theta), np.sin(self.theta)]),  np.array([self.x_goal, self.y_goal]), np.array([u_water, v_water]))), self.reward, self.sum_reward, self.done, self.steps_count, self.all_actions
+        
+        else:
+            return np.concatenate((self.pos, np.array([np.cos(self.theta), np.sin(self.theta)]),  np.array([self.x_goal, self.y_goal]))), self.reward, self.sum_reward, self.done, self.steps_count, self.all_actions
+        
+    def reset(self):
+
+        self.rudder = 0
+        self.thrust = 0  
+        self.pos = np.array([0, 0, 0])
+        self.done = False
+        self.goal_reached = False
+        self.steps_count = 0
+        self.sum_reward = 0
+        self.t = 0
+        # Statement: by randomly initilizing theta, we learn beter how to adjust/turn round to reach goal
+        self.theta = np.random.uniform(0, 2*np.pi) # if statement false: self.theta = 0
+        self.dir_goal = normalize_vector_from_point(self.x_goal, self.y_goal, 0, 0)
+        self.straight = False
+
+        if self.ocean: 
+            # TODO: Add noise to observation if algo works fine with perfect estimate of wave and winds speed
+
+            # TODO: separate wind and water component
+
+            u_water, v_water, _ = self.water_speed(self.pos)
+            return np.concatenate((self.pos, np.array([np.cos(self.theta), np.sin(self.theta)]),  np.array([self.x_goal, self.y_goal]), np.array([u_water, v_water])))
+        else:
+            return np.concatenate((self.pos, np.array([np.cos(self.theta), np.sin(self.theta)]),  np.array([self.x_goal, self.y_goal])))
+
+
+# env = FluidMechanicsEnv(a=0.1, # range 0.1, 0.5, 1, 2, 5
+#                         T=1, # wave period, range 10 to 20
+#                         k=0.1, #wave number m^-1: 0.05 to 0.5
+#                         Ux=0, #wind x component: -2 to 2
+#                         Uy=0, 
+#                         alpha=1, # vertical wind decay: around 1
+#                         sigma=0, # noise wind parameter: around 10% wind speed
+#                         x_goal=4, 
+#                         y_goal=4, 
+#                         pos0=np.array([0, 0, 0]), 
+#                         theta0=0,
+#                         dist_threshold=0.2, 
+#                         max_steps=200, 
+#                         ocean=False, # if false: still water env. If true: ocean like env
+#                         dt=1, # time step. For now keep 1, we could go smaller
+#                         max_thrust_speed = 1# Robot's speed at 100% thrust 
+#                         )
